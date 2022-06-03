da862133e5e9e550c5223023895a45fd1c1328d8
Vision-Paudel <visionpaudel@gmail.com>
2022-06-03 17:24:49 -0400

    Finished Services

--- Fusion/src/main/java/services/OrdersService.java
+++ Fusion/src/main/java/services/OrdersService.java
@@ -1,6 +1,23 @@
 package services;
 
+import models.Orders;
+
+import java.util.List;
+
 public interface OrdersService {
 
+    public Orders getOrder(int oID);
+
+    public Orders getOrderByUserID(int uID);
+
+    public List<Orders> getAllOrders();
+
+    public Orders addOrder(Orders o);
+
+    public Orders updateOrders(Orders change);
+
+    public Orders deleteOrders(int oID);
+
+    public Orders deleteOrdersByUserID(int uID);
 
 }
--- Fusion/src/main/java/services/OrdersServiceImpl.java
+++ Fusion/src/main/java/services/OrdersServiceImpl.java
@@ -1,4 +1,51 @@
 package services;
 
-public class OrdersServiceImpl {
+import models.Orders;
+import repositories.OrdersRepo;
+
+import java.util.List;
+
+public class OrdersServiceImpl implements OrdersService{
+
+    private OrdersRepo or;
+
+    public OrdersServiceImpl(OrdersRepo or) {
+        this.or = or;
+    }
+
+    @Override
+    public Orders getOrder(int oID) {
+        return or.getOrder(oID);
+    }
+
+    @Override
+    public Orders getOrderByUserID(int uID) {
+        return or.getOrderByUserID(uID);
+    }
+
+    @Override
+    public List<Orders> getAllOrders() {
+        return or.getAllOrders();
+    }
+
+    @Override
+    public Orders addOrder(Orders o) {
+        return or.addOrder(o);
+    }
+
+    @Override
+    public Orders updateOrders(Orders change) {
+        return or.updateOrders(change);
+    }
+
+    @Override
+    public Orders deleteOrders(int oID) {
+        return or.deleteOrders(oID);
+    }
+
+    @Override
+    public Orders deleteOrdersByUserID(int uID) {
+        return or.deleteOrdersByUserID(uID);
+    }
+
 }
