e8f446bb08b4d179ed85d7632e3186663bc1be89
Vision-Paudel <visionpaudel@gmail.com>
2022-06-06 09:03:24 -0400

    Setup OrdersController and OrderDetailsController. Need to fill it today!

--- Fusion/src/main/java/controllers/OrderDetailsController.java
+++ Fusion/src/main/java/controllers/OrderDetailsController.java
@@ -1,6 +1,17 @@
 package controllers;
 
+import com.google.gson.Gson;
+import services.OrderDetailsService;
+
 public class OrderDetailsController {
+
+    private OrderDetailsService ods;
+    private Gson gson = new Gson();
+
+    public OrderDetailsController(OrderDetailsService ods) {
+        this.ods = ods;
+    }
+
 
 
 
--- Fusion/src/main/java/controllers/OrdersController.java
+++ Fusion/src/main/java/controllers/OrdersController.java
@@ -1,8 +1,22 @@
 package controllers;
 
+import com.google.gson.Gson;
+import io.javalin.http.Handler;
+import services.OrderDetailsService;
+import services.OrdersService;
+
 public class OrdersController {
 
+    private OrdersService os;
+    private Gson gson = new Gson();
 
+    public OrdersController(OrdersService os) {
+        this.os = os;
+    }
+
+    public Handler getAllOrders = (context) -> {
+
+    };
 
 
 
