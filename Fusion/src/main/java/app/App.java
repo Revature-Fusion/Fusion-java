f1c63aa68f038503c2ff77f84093c1b7a0188553
Vision-Paudel <visionpaudel@gmail.com>
2022-06-06 17:43:05 -0400

    Added App.java file

---
+++ Fusion/src/main/java/app/App.java
@@ -0,0 +1,57 @@
+package app;
+
+import controllers.OrderDetailsController;
+import controllers.OrdersController;
+import io.javalin.Javalin;
+import repositories.OrderDetailsRepo;
+import repositories.OrderDetailsRepoImpl;
+import repositories.OrdersRepo;
+import repositories.OrdersRepoImpl;
+import services.OrderDetailsService;
+import services.OrderDetailsServiceImpl;
+import services.OrdersService;
+import services.OrdersServiceImpl;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Javalin app = Javalin.create(config -> config.enableCorsForAllOrigins());
+
+        establishRoutes(app);
+
+        app.start();
+
+    }
+
+    private static void establishRoutes(Javalin app) {
+
+        OrdersRepo or = new OrdersRepoImpl();
+        OrdersService os = new OrdersServiceImpl(or);
+        OrdersController oc = new OrdersController(os);
+
+        OrderDetailsRepo odr = new OrderDetailsRepoImpl();
+        OrderDetailsService ods = new OrderDetailsServiceImpl(odr);
+        OrderDetailsController odc = new OrderDetailsController(ods);
+
+        // For orders
+        app.get("/orders", oc.getAllOrders);
+        app.get("/orders/:oID", oc.getOrderById);
+        app.get("/ordersByUID/:uID", oc.getOrdersByUserID); // To get orders by userID
+        app.post("/orders/", oc.createOrder);
+        app.put("/orders/:oID", oc.updateOrder);
+        app.delete("/orders/:oID", oc.deleteOrder);
+        app.delete("/ordersByUID/:uID", oc.deleteOrdersByUserId);   // To delete orders by userID
+
+        // For order_details
+        app.get("/orderDetails", odc.getAllOrderDetails);
+        app.get("/orderDetails/:oDID", odc.getOrderDetails);
+        app.get("/orderDetailsByOID/:oID", odc.getOrderDetailsByOrderID); // To get order details by orderID
+        app.post("/orderDetails/", odc.createOrderDetails);
+        app.put("/orderDetails/:oDID", odc.updateOrderDetails);
+        app.delete("/orderDetails/:oDID", odc.deleteOrderDetails);
+        app.delete("/orderDetailsByOID/:oID", odc.deleteOrderDetailsByOrderID);   // To delete order details by orderID
+
+    }
+
+}
