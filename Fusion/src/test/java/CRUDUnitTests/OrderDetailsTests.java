33c8bcb3cd5343878fe5c960ba2215d9801ab6cd
Vision-Paudel <visionpaudel@gmail.com>
2022-06-08 11:26:50 -0400

    Tests for Orders is complete.

--- Fusion/src/test/java/CRUDUnitTests/OrderDetailsTests.java
+++ Fusion/src/test/java/CRUDUnitTests/OrderDetailsTests.java
@@ -1,8 +1,22 @@
 package CRUDUnitTests;
 
+import repositories.OrderDetailsRepo;
+import repositories.OrderDetailsRepoImpl;
+import repositories.OrdersRepo;
+import repositories.OrdersRepoImpl;
+import services.OrderDetailsService;
+import services.OrderDetailsServiceImpl;
+import services.OrdersService;
+import services.OrdersServiceImpl;
+
 public class OrderDetailsTests {
 
+    OrdersRepo or = new OrdersRepoImpl();
+    OrdersService os = new OrdersServiceImpl(or);
+    OrderDetailsRepo odr = new OrderDetailsRepoImpl();
+    OrderDetailsService ods = new OrderDetailsServiceImpl(odr);
 
+    
 
 
 }
--- Fusion/src/test/java/CRUDUnitTests/OrdersTests.java
+++ Fusion/src/test/java/CRUDUnitTests/OrdersTests.java
@@ -1,10 +1,15 @@
 package CRUDUnitTests;
 
+import models.OrderDetails;
 import models.Orders;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import repositories.OrderDetailsRepo;
+import repositories.OrderDetailsRepoImpl;
 import repositories.OrdersRepo;
 import repositories.OrdersRepoImpl;
+import services.OrderDetailsService;
+import services.OrderDetailsServiceImpl;
 import services.OrdersService;
 import services.OrdersServiceImpl;
 
@@ -14,26 +19,28 @@ public class OrdersTests {
 
     OrdersRepo or = new OrdersRepoImpl();
     OrdersService os = new OrdersServiceImpl(or);
+    OrderDetailsRepo odr = new OrderDetailsRepoImpl();
+    OrderDetailsService ods = new OrderDetailsServiceImpl(odr);
 
     @Test
     public void testGetOrder(){
-        Orders orders = os.getOrder(1);
-        Orders expectedOrders = new Orders(1, 1,1654613952000l, 1);
+        Orders orders = os.getOrder(999);
+        Orders expectedOrders = new Orders(999, 2,1654613952000l, 1);
         Assertions.assertEquals(expectedOrders, orders);
     }
 
     @Test
     public void  getOrderByUserID(){
-        List<Orders> orders = os.getOrderByUserID(1);
+        List<Orders> orders = os.getOrderByUserID(2);
         Assertions.assertTrue(orders.size() > 0 && orders.size() < 150);
-        Assertions.assertEquals(orders.get(0), new Orders(1,1,1654613952000l,1));
+        Assertions.assertEquals(orders.get(orders.size()-1), new Orders(999, 2,1654613952000l, 1));
     }
 
     @Test
     public void getAllOrders(){
         List<Orders> orders = os.getAllOrders();
         Assertions.assertTrue(orders.size() > 0 && orders.size() < 150);
-        Assertions.assertEquals(orders.get(0), new Orders(1,1,1654613952000l,1));
+        Assertions.assertEquals(orders.get(orders.size()-1), new Orders(999,2,1654613952000l,1));
     }
 
     @Test
@@ -79,17 +86,21 @@ public class OrdersTests {
 
     @Test
     public void testDeleteLoginByUserID(){
-//        Need to recreate the orders after deleting them based on userID so it doesn't affect the database
+          Orders o = new Orders(3, 1,1654633955000l,1);
 
-//        Orders o = new Orders(3, 1,1654613955000l,1);
-//
-//        Orders addedOrders = os.addOrder(o);
-//
-//        Orders deletedOrders = os.deleteOrdersByUserID(addedOrders.geuID());
-//        Assertions.assertEquals(addedOrders, deletedOrders);
-//
-//        Orders deletedOrders2 = os.deleteOrders(addedOrders.getoID());
-//        Assertions.assertNull(deletedOrders2);
+          Orders addedOrders = os.addOrder(o);
+
+          Orders deletedOrders = os.deleteOrdersByUserID(addedOrders.getuID());
+          Assertions.assertNotNull(deletedOrders, "Should not be null");
+
+          Orders deletedOrders2 = os.deleteOrdersByUserID(addedOrders.getuID());
+          Assertions.assertNull(deletedOrders2, "Should be null");
+
+          Orders o1 = new Orders(1, 1,1654613952000l, 1);
+          o1 = os.addOrder(o1);
+
+          ods.addOrderDetails( new OrderDetails(1,o1.getoID(),2,2) );
+          ods.addOrderDetails( new OrderDetails(2,o1.getoID(),3,2) );
     }
 
 
